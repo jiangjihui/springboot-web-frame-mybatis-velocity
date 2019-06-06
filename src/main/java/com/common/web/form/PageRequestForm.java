9dc70bf1304947702a19290d396d67ee67ca4aa8
jjh <xqlsrjjjh@outlook.com>
2019-06-06 18:03:20 +0800

    添加分页类注释

--- src/main/java/com/common/web/form/PageRequestForm.java
+++ src/main/java/com/common/web/form/PageRequestForm.java
@@ -7,24 +7,21 @@ import java.util.Map;
 
 /**
  * 简单分页查询类
+ * @author jjh
  */
 public class PageRequestForm {
 
-    private int offset;
+    /** 页码 */
     private int pageNum;
+    /** 每页大小 */
     private int pageSize;
+    /** 排序字段 */
     private String orderColumn;
+    /** 排序方式：asc 升序/desc 降序 */
     private String sort;
 
+    /** 筛选（过滤）参数 */
     private Map<String,Object> params;
-
-    public int getOffset() {
-        return offset;
-    }
-
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
 
     public int getPageNum() {
         return pageNum;
