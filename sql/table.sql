6fe5bbee57b9e91b03b79b098c99987c01394a23
jjh <xqlsrjjjh@outlook.com>
2019-06-06 17:53:33 +0800

    添加sql文件

---
+++ sql/table.sql
@@ -0,0 +1,16 @@
+
+-- 通知公告表（可以用于测试代码生成）
+CREATE TABLE `sys_publish` (
+  `id` varchar(40) NOT NULL COMMENT '唯一ID',
+  `notice_title` varchar(50) NOT NULL COMMENT '公告标题',
+  `notice_type` char(1) NOT NULL COMMENT '公告类型（1通知 2公告）',
+  `notice_content` varchar(2000) DEFAULT NULL COMMENT '公告内容',
+  `status` char(1) DEFAULT '0' COMMENT '公告状态（0正常 1关闭）',
+  `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='通知公告表';
+
